78f1a2d8bc6ecc9935e4aecc163f35d696d43799
Miguel Ramos <return.infox@gmail.com>
2023-09-27 19:45:39 -0300

    app de rick and morty navbar

--- proyecto-04/js/app.js
+++ proyecto-04/js/app.js
@@ -1,23 +1,21 @@
 //* Inicio de las peticiones hacia el API DE RICK AND MORTY
 //* en "results" estan los personajes
-// * 300ms promedio, es decir se toma su tiempo
+//* 300ms promedio, es decir se toma su tiempo
 
 const getApiRickAndMortyFull = async () => {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
-  return data; // {} completo de la respuesta 
+  return data; //* {} completo de la respuesta 
 };
-
 // async function getApiRickAndMortyFull() {
 //   const response = await fetch("https://rickandmortyapi.com/api/character");
 //   const data = await response.json();
 //   return data; // {} completo de la respuesta 
 // }
-
 //* necesitamos una función que me devuelva los resultados
 const init = async () => {
   const apiRickAndMortyFull = await getApiRickAndMortyFull();
-  const getCharacters = apiRickAndMortyFull.results;
+  const getCharacters = apiRickAndMortyFull.results; //* nos traemos el vector o array "results"
   console.log(getCharacters);
 };
 
